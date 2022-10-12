44acc551d08f026c083033a2c0ee845e83e54ea8
Spiderbezno <j2agm@hotmail.com>
2022-10-12 18:59:06 +0200

    Fin sección 8

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -7,6 +7,7 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { throws } from 'assert';
 import { isValidObjectId, Model } from 'mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
@@ -28,9 +29,12 @@ export class PokemonService {
     }
   }
 
-  async findAll() {
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
     const filter = {};
-    return await this.pokemonModel.find(filter);
+    return await this.pokemonModel.find(filter).limit(limit).skip(offset).sort({
+      no: 1,
+    });
   }
 
   async findOne(id: string) {
